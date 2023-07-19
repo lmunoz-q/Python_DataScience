971fb3e6bd3dedfb07be739cbcc0057c4881613c
aorus <lmunoz-q@student.42.fr>
2023-07-19 15:27:46 +0200

    ex02

---
+++ 0_Starting/ex02/find_ft_type.py
@@ -0,0 +1,17 @@
+def all_thing_is_obj(obj: any) -> int:
+    if isinstance(obj, list):
+        print("List:", type(obj))
+    elif isinstance(obj, tuple):
+        print("Tuple:", type(obj))
+    elif isinstance(obj, set):
+        print("Set:", type(obj))
+    elif isinstance(obj, dict):
+        print("Dict:", type(obj))
+    elif isinstance(obj, str):
+        if obj == "Brian":
+            print(obj, "is in the kitchen:", type(obj))
+        else:
+            print("String:", type(obj))
+    else:
+        print("Type not found")
+    return 42
