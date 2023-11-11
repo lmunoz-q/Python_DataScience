e82a167cad94dca34e3e45572880ff945c6bd8a5
aorus <lmunoz-q@student.42.fr>
2023-11-11 14:52:14 +0100

    nothing to do

--- 0_Starting/ex04/whatis.py
+++ 0_Starting/ex04/whatis.py
@@ -1,16 +1,19 @@
 import sys
 
-# Check the number of arguments
-if len(sys.argv) != 2:
-    raise AssertionError("more than one argument is provided")
-# Try to parse the argument as an integer
-try:
-    arg = int(sys.argv[1])
-except ValueError:
-    raise AssertionError("argument must be an integer")
-# Check if the integer is even or odd
-else:
-    if int(sys.argv[1]) % 2 == 0:
-        print("I'm Even.")
-    else:
-        print("I'm Odd.")
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        raise AssertionError("more than one argument is provided")
+    elif len(sys.argv) == 1:
+        sys.exit(0)
+
+    try:
+        num = int(sys.argv[1])
+        if not isinstance(num, int):
+            raise ValueError
+
+        if num % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except ValueError:
+        raise AssertionError("argument is not an integer")
