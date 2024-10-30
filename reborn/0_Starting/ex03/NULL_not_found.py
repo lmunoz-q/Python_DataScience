9f705ceb6e9f2d0f0ffa7c036d5a1fe929ebdcfa
lmunoz_region_idf <lmunoz@student.42.fr>
2024-10-30 21:16:11 +0100

    finally found

--- reborn/0_Starting/ex03/NULL_not_found.py
+++ reborn/0_Starting/ex03/NULL_not_found.py
@@ -3,12 +3,12 @@ def NULL_not_found(obj: any) -> int:
         print(f"Nothing: {obj} <class 'NoneType'>")
     elif isinstance(obj, float) and obj != obj:
         print(f"Cheese: {obj} <class 'float'>")
-    elif obj == 0:
-        print(f"Zero: {obj} <class 'int'>")
-    elif isinstance(obj, str) and obj == "":
-        print(f"Empty: {obj} <class 'str'>")
     elif obj is False:
         print(f"Fake: {obj} <class 'bool'>")
+    elif isinstance(obj, str) and obj == "":
+        print(f"Empty: {obj} <class 'str'>")
+    elif obj == 0:
+        print(f"Zero: {obj} <class 'int'>")
     else:
         print("Type not Found")
     return 1
