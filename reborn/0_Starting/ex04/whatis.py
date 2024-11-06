3aa9b1646ee95b7c5fca2e3616bbc4c17e9ccb15
aorus <lmunoz-q@student.42.fr>
2024-11-06 23:22:58 +0100

    what is done

--- reborn/0_Starting/ex04/whatis.py
+++ reborn/0_Starting/ex04/whatis.py
@@ -1,3 +1,13 @@
 import sys
 
-print(f"{sys.argv[0]}")
+assert len(sys.argv) < 3, "more than one argument is provided"
+
+if len(sys.argv) == 2:
+    try:
+        number = int(sys.argv[1])
+    except ValueError:
+        raise AssertionError("argument is not an integer")
+    if number % 2 == 0:
+        print("I'm Even")
+    else:
+        print("I'm Odd.")
