4528019d35123e92b5e637caed51d36cc91e7bd1
lmunoz-q <lmunoz-q@github.com>
2024-11-09 22:50:23 +0100

    optification

--- reborn/0_Starting/ex05/building.py
+++ reborn/0_Starting/ex05/building.py
@@ -1,18 +1,19 @@
 import sys
 
-def count_upper(str text) -> int
-    x = 0
-    for i in text:
-        if i.isupper():
-            x += 1
-    return x
+def count_upper(text: str) -> int:
+    return sum(1 for c in text if c.isupper())
+
+def count_lower(text: str) -> int:
+    return sum(1 for c in text if c.islower())
 
 def main():
     assert len(sys.argv) < 3, "more than one argument provied"
     if len(sys.argv) == 1:
         print("What is the text to count ?")
     else:
-        print(f"The text contains {count_upper(sys.argv[1])} characters:")
+        print(f"The text contains {len(sys.argv[1])} characters:")
+        print(f"{count_upper(sys.argv[1])} upper letters")
+        print(f"{count_lower(sys.argv[1])} lower letters")
 
 if __name__ == "__main__":
     main()
