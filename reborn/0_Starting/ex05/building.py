72c082aacd7acc2dca7969bdbed212a4b50917a1
lmunoz-q <lmunoz-q@github.com>
2024-11-09 23:23:02 +0100

    ll

--- reborn/0_Starting/ex05/building.py
+++ reborn/0_Starting/ex05/building.py
@@ -6,6 +6,12 @@ def count_upper(text: str) -> int:
 def count_lower(text: str) -> int:
     return sum(1 for c in text if c.islower())
 
+def count_number(text: str) -> int:
+    return sum(1 for c in text if c.isdigit())
+
+def count_spaces(text: str) -> int:
+    return sum(1 for c in text if c.isspace())
+
 def main():
     assert len(sys.argv) < 3, "more than one argument provied"
     if len(sys.argv) == 1:
@@ -14,6 +20,8 @@ def main():
         print(f"The text contains {len(sys.argv[1])} characters:")
         print(f"{count_upper(sys.argv[1])} upper letters")
         print(f"{count_lower(sys.argv[1])} lower letters")
+        print(f"{count_number(sys.argv[1])} numbers")
+        print(f"{count_spaces(sys.argv[1])} spaces")
 
 if __name__ == "__main__":
     main()
