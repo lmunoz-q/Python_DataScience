ebac9d780cf37fc58f761d5a0418cd9bd5bace4c
lmunoz-q <lmunoz-q@github.com>
2024-11-10 14:23:03 +0100

    gp

---
+++ reborn/0_Starting/ex06/ft_filter.py
@@ -0,0 +1,16 @@
+def ft_filter(function, iterable):
+    """
+    Filter elements of an iterable with  fucntion wigoutusing th built-in filter function.
+
+    Args:
+        function (callable): A function that returns True or Flase for earch element.
+        iterable (iteratable): The iterable to be filtered.
+
+    Returns:
+        list: A list containing the elements from iterable for which function returns True.
+    """
+    x[]
+    for i in iterable:
+        if function(i):
+            x.append(i)
+    return x
