24b79957c633e236d5a4f2143fb4a1988cb8d12d
aorus <lmunoz-q@student.42.fr>
2024-11-18 17:26:54 +0100

    tests

--- reborn/0_Starting/ex06/filterstring.py
+++ reborn/0_Starting/ex06/filterstring.py
@@ -1,11 +1,19 @@
 import sys
 
+def ft_filter(function, iterable):
+    return [x for x in iterable if function(x)]
+
+def is_ok(text, number):
+    if len(text) > number:
+        return text
 
 def main():
     assert len(sys.argv) == 3, "the arguments are bad"
-    try:
-        argv[1]
+    liste = sys.argv[1].split()
+    chaine = sys.argv[1]
 
+    print(f"{liste}")
+    print(f"{chaine}")
 
 if __name__ == "__main__":
     main()
