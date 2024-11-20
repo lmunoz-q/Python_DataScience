d288579078fd30725c9f0018c2209c60c58c6c02
aorus <lmunoz-q@student.42.fr>
2024-11-20 15:13:20 +0100

    not done

--- reborn/0_Starting/ex06/filterstring.py
+++ reborn/0_Starting/ex06/filterstring.py
@@ -1,25 +1,22 @@
 import sys
+from ft_filter import ft_filter
 
-def ft_filter(function, iterable):
-    return [x for x in iterable if function(x)]
 
 def is_ok(text, number):
-    if len(text) > number:
-        return True
-    else:
-        return False
+    return len(text) > number
+
 
 def main():
     assert len(sys.argv) == 3, "the arguments are bad"
+    try:
+        int(sys.argv[2])
+    except:
+        print("the arguments are bad")
+    number = int(sys.argv[2])
     liste = sys.argv[1].split()
-    chaine = sys.argv[1]
 
-    print(f"{liste}")
-    print(f"{chaine}")
-    print(f"{len(liste)}")
-    print(f"{len(chaine)}")
+    print(ft_filter(lambda word: is_ok(word, number), liste))
 
-    print(ft_filter(lambda word: is_ok(word, int(sys.argv[2])), liste))
 
 if __name__ == "__main__":
     main()
--- reborn/0_Starting/ex06/ft_filter.py
+++ reborn/0_Starting/ex06/ft_filter.py
@@ -1,12 +1,15 @@
 def ft_filter(function, iterable):
     """
-    Filter elements of an iterable with  fucntion wigoutusing th built-in filter function.
+    Filter elements of an iterable with  fucntion wigoutusing th built-in
+    filter function.
 
     Args:
-        function (callable): A function that returns True or Flase for earch element.
+        function (callable): A function that returns True or Flase for earch
+        element.
         iterable (iteratable): The iterable to be filtered.
 
     Returns:
-        list: A list containing the elements from iterable for which function returns True.
+        list: A list containing the elements from iterable for which function
+        returns True.
     """
     return [x for x in iterable if function(x)]
