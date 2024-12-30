3d9cdbe7900598b466e5299533b9e783810ff46b
lmunoz-q <lmunoz-q@student.42.fr>
2024-12-30 18:43:57 +0100

    ex00 done

--- reborn/ex00/give_bmi.py
+++ reborn/ex00/give_bmi.py
@@ -1,4 +1,10 @@
+import sys
+
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    assert len(height) == len(weight), "the length of two list must be equal"
+    for x, y in zip(height, weight):
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raiseTypeError("Both list must contain only integers or floats.`")
     return [w / (h ** 2) for h, w in zip(height, weight)]
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
