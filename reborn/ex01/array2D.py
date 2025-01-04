4cc22f385cf9f4c200f213da1a103e4a7ffa5f7d
lmunoz-q <lmunoz-q@student.42.fr>
2025-01-04 22:27:10 +0100

    working process

---
+++ reborn/ex01/array2D.py
@@ -0,0 +1,11 @@
+family = [[1.80, 78.4], [2.15, 102.7], [2.10, 98.5], [1.88, 75.2]]
+
+print(family)
+print("family[0]")
+print(family[0])
+print("family[0][0]")
+print(family[0][0])
+print("family[1][0]")
+print(family[1][0])
+print("family[1][1]")
+print(family[1][1])
