6e0d6e56416d2ee80384b3e018ae05913519f94f
lmunoz-q <lmunoz-q@student.42.fr>
2025-01-04 22:06:03 +0100

    ex00 done

--- reborn/ex00/give_bmi.py
+++ reborn/ex00/give_bmi.py
@@ -4,8 +4,13 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
     assert len(height) == len(weight), "the length of two list must be equal"
     for x, y in zip(height, weight):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raiseTypeError("Both list must contain only integers or floats.`")
+            raiseTypeError("Both list must contain only integers or floats.")
     return [w / (h ** 2) for h, w in zip(height, weight)]
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    for x in bmi:
+        if not isinstance(x, (int, float)):
+            raiseTypeError("List must contain only integers or floats.")
+    if not isinstance(limit, int):
+        raiseTypeError("'limit' must be a integer")
     return [x > limit for x in bmi]
