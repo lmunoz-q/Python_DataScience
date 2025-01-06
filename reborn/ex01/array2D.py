d93147084298b58c680679109c6ab123d2fdde4d
lmunoz-q <lmunoz-q@student.42.fr>
2025-01-06 15:24:58 +0100

    lolilol

--- reborn/ex01/array2D.py
+++ reborn/ex01/array2D.py
@@ -1,16 +1,13 @@
-family = [[1.80, 78.4],
-          [2.15, 102.7],
-          [2.10, 98.5],
-          [1.88, 75.2]]
+def slice_me(family: list, start: int, end: int) -> list:
 
-lines = len(family)
-cols = len(family[0])
+    lines = len(family)
+    cols = len(family[0])
 
-sFamily = family[1:3]
+    sFamily = family[start:end]
 
-sLines = len(sFamily)
-sCols = len(sFamily[0])
-print(family)
-print(f"My shape is : ({lines}, {cols})")
-print(sFamily)
-print(f"My new shape is : ({sLines}, {sCols})")
+    sLines = len(sFamily)
+    sCols = len(sFamily[0])
+    print(f"My shape is : ({lines}, {cols})")
+    print(f"My new shape is : ({sLines}, {sCols})")
+    print("IL FAUT FAIRE GESTION DERREUR ")
+    return sFamily
