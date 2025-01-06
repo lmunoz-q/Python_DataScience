af35a8595397b97d4ffbed5188f6c36c2bb65101
lmunoz-q <lmunoz-q@student.42.fr>
2025-01-06 15:04:48 +0100

    jeogj

--- reborn/ex01/array2D.py
+++ reborn/ex01/array2D.py
@@ -1,11 +1,16 @@
-family = [[1.80, 78.4], [2.15, 102.7], [2.10, 98.5], [1.88, 75.2]]
+family = [[1.80, 78.4],
+          [2.15, 102.7],
+          [2.10, 98.5],
+          [1.88, 75.2]]
 
+lines = len(family)
+cols = len(family[0])
+
+sFamily = family[1:3]
+
+sLines = len(sFamily)
+sCols = len(sFamily[0])
 print(family)
-print("family[0]")
-print(family[0])
-print("family[0][0]")
-print(family[0][0])
-print("family[1][0]")
-print(family[1][0])
-print("family[1][1]")
-print(family[1][1])
+print(f"My shape is : ({lines}, {cols})")
+print(sFamily)
+print(f"My new shape is : ({sLines}, {sCols})")
