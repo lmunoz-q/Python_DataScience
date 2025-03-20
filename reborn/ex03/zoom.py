3c93ca28290ce146ec667dcc75fa31a5caddb733
aorus <lmunoz-q@student.42.fr>
2025-03-20 17:46:14 +0100

    nah

--- reborn/ex03/zoom.py
+++ reborn/ex03/zoom.py
@@ -1,8 +1,35 @@
 from load_image import ft_load
 from PIL import Image
+import matplotlib.pyplot as plt
 import numpy as np
 
-img_array = ft_load("animal.jpeg")
 
-if img_array is not None:
-    img = Image.fromarray(np.array(ft_load))
+def on_zoom(event, img_array):
+    current_xlim = plt.gca().get_xlim()
+    current_ylim = plt.gca().get_ylim()
+
+    if current_xlim != on_zoom.prev_xlim or current_ylim != on_zoom.prev_ylim:
+        y_min, y_max = current_ylim
+        y_min_idx = int(np.floor(y_min))
+        y_max_idx = int(np.floor(y_max))
+        print(f"New shape after slicing: {img_array[y_min_idx]}")
+        on_zoom.prev_xlim = current_xlim
+        on_zoom.prev_ylim = current_ylim
+
+
+on_zoom.prev_xlim = None
+on_zoom.prev_ylim = None
+
+pathImg = "animal.jpeg"
+print(ft_load(pathImg))
+
+#img = np.asarray(Image.open(pathImg))
+img = Image.open(pathImg)
+img_array = np.array(img)
+
+plt.imshow(img)
+
+plt.gcf().canvas.mpl_connect('draw_event', lambda event: on_zoom(event, img_array))
+
+
+plt.show()
