a924c4c6703a9714da5e8969cb32687b5c75adc2
aorus <lmunoz-q@student.42.fr>
2025-03-27 17:37:56 +0100

    bruh

--- reborn/ex03/load_image.py
+++ reborn/ex03/load_image.py
@@ -1,6 +1,22 @@
 from PIL import Image
-
 import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    cols = len(family[0])
+    for i in family:
+        if not isinstance(i, list):
+            raise AssertionError("'family' must be a array2D")
+        for x in i:
+            if not isinstance(x, (int | float)):
+                raise AssertionError("array2D must contain integers or floats")
+        if cols != len(i):
+            raise AssertionError("List must contain lines with columns equal")
+
+    sFamily = family[start:end]
+
+    return sFamily
+
 
 def ft_load(path: str):
     try:
--- reborn/ex03/zoom.py
+++ reborn/ex03/zoom.py
@@ -1,17 +1,23 @@
-from load_image import ft_load
-from array2D import slice_me
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-
+from load_image import ft_load, slice_me
 
 
 pathImg = "animal.jpeg"
-#print(ft_load(pathImg))
+img_array = ft_load(pathImg)
 
-img = Image.open(pathImg)
-img_array = np.array(img)
-print(img_array)
+if img_array is not None:
+    print(img_array)
 
-plt.imshow(img)
-plt.show()
+    img = Image.open(pathImg)
+    img_array_full = np.array(img)
+
+    channel = img_array_full[:, :, 0]
+    z_image = slice_me(channel.tolist(), 0, 400)
+    z_image = [row[:400] for row in z_image]
+    print(f"New shape after slicing: ({len(z_image)}, {len(z_image[0])})")
+    print(np.array(channel)[0])
+
+    plt.imshow(z_image, cmap="gray")
+    plt.show()
