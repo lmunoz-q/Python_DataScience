265130583a6ca387060dc4cfb297ff25d602a811
aorus <lmunoz-q@student.42.fr>
2025-04-06 20:08:22 +0200

    norme

--- reborn/ex00/give_bmi.py
+++ reborn/ex00/give_bmi.py
@@ -1,15 +1,15 @@
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
-    assert len(height) == len(weight), "the length of two list must be equal"
-    for x, y in zip(height, weight):
+def give_bmi(h: list[int | float], w: list[int | float]) -> list[int | float]:
+    assert len(h) == len(w), "the length of two list must be equal"
+    for x, y in zip(h, w):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raiseTypeError("Both list must contain only integers or floats.")
-    return [w / (h ** 2) for h, w in zip(height, weight)]
+            raise ("Both list must contain only integers or floats.")
+    return [w / (h ** 2) for h, w in zip(h, w)]
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     for x in bmi:
         if not isinstance(x, (int, float)):
-            raiseTypeError("List must contain only integers or floats.")
+            raise ("List must contain only integers or floats.")
     if not isinstance(limit, int):
-        raiseTypeError("'limit' must be a integer")
+        raise ("'limit' must be a integer")
     return [x > limit for x in bmi]
--- reborn/ex02/load_image.py
+++ reborn/ex02/load_image.py
@@ -1,6 +1,6 @@
 from PIL import Image
-
 import numpy as np
+
 
 def ft_load(path: str):
     try:
