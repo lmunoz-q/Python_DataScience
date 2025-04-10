16601666ec0bfffe1d131830048ee9dfd56b597f
aorus <lmunoz-q@student.42.fr>
2025-04-10 13:50:55 +0200

    preupre zoom

--- 1_Array/ex03/zoom.py
+++ 1_Array/ex03/zoom.py
@@ -3,19 +3,37 @@ import numpy as np
 from load_image import ft_load
 
 
-pathImg = "animal.jpeg"
-img_array = ft_load(pathImg)
+def ft_zoom(path: str) -> None:
+    """
+    Loads an image, extracs a 400x400 zoom on the red channel,
+    prints the shape, preview of the array and displays it in gray.
 
-if img_array is not None:
-    channel = img_array[:, :, 0]
-    zoom = channel[100:500, 450:850]
-    zoom = np.array(zoom)
-    if zoom.ndim == 2:
-        zoom = np.expand_dims(zoom, axis=-1)
-    print(f"New shape after slicing: {zoom.shape}")
-    print(zoom[0:1, 0:3])
-    print("   ...")
-    print(zoom[-1:, -3:])
+    Parameters
+    ----------
+    path : str
+        Path to the image file.
+    """
+    img_array = ft_load(path)
 
-    plt.imshow(zoom, cmap="gray")
-    plt.show()
+    if img_array is not None:
+        channel = img_array[:, :, 0]
+        zoom = channel[100:500, 450:850]
+        zoom = np.array(zoom)
+        if zoom.ndim == 2:
+            zoom = np.expand_dims(zoom, axis=-1)
+        print(f"New shape after slicing: {zoom.shape}")
+        print(zoom[0:1, 0:3])
+        print("   ...")
+        print(zoom[-1:, -3:])
+
+        plt.imshow(zoom, cmap="gray")
+        plt.show()
+
+
+def main():
+    pathImg = "animal.jpeg"
+    ft_zoom(pathImg)
+
+
+if __name__ == "__main__":
+    main()
