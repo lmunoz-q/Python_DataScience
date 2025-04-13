8e22e0cc1d91528e0088760a0e7738d4e688cfba
aorus <lmunoz-q@student.42.fr>
2025-04-13 23:30:42 +0200

    ft_zoom + ft rotate

--- 1_Array/ex04/rotate.py
+++ 1_Array/ex04/rotate.py
@@ -1,11 +1,37 @@
 from load_image import ft_load
 import matplotlib.pyplot as plt
-from numpy as np
+import numpy as np
+
+
+def ft_zoom(path: str) -> np.ndarray:
+    img_array = ft_load(path)
+
+    if img_array is not None:
+        channel = img_array[:, :, 0]
+        zoom = channel[100:500, 450:850]
+        zoom = np.array(zoom)
+        if zoom.ndim == 2:
+            zoom = np.expand_dims(zoom, axis=-1)
+
+    return zoom
+
+
+def ft_rotate(img: np.ndarray) -> None:
+    if isinstance(img, np.ndarray):
+        print(f"The shape if image is: {img.shape}")
+        print(img[0:1, 0:3])
+        print("   ...")
+        print(img[-1:, -3:])
+        print(f"New shape after Transpose: {img.shape}")
+        plt.imshow(img, cmap="gray")
+        plt.show()
 
 
 def main():
     path = "animal.jpeg"
-    print("lol")
+    img = ft_zoom(path)
+    ft_rotate(img)
+
 
 if __name__ == "__main__":
     main()
