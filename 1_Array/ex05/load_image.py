1228c364bccdc6b5dee9e7ca3da628277c847e62
aorus <lmunoz-q@student.42.fr>
2025-04-17 01:58:02 +0200

    load image done

---
+++ 1_Array/ex05/load_image.py
@@ -0,0 +1,38 @@
+from PIL import Image
+import numpy as np
+
+
+def ft_load(path: str):
+    """
+    Load an image file and return its content as a NumPy array.
+
+    Parameters
+    ----------
+    path : str
+        Path to the image file (JPEG or JPG).
+
+    Returns
+    -------
+    np.ndarray | None
+        The image converted into a NumPy array, or None if an error occurred.
+
+    Raises
+    ------
+    ValueError
+        If the image format is not JPEG or JPG.
+
+    Notes
+    -----
+    The function also prints the shape of the image if loading is successful.
+    """
+    try:
+        img = Image.open(path)
+        if img.format not in ["JPEG", "JPG"]:
+            raise ValueError("Error: ONly JPG and JPEG are allowed")
+        img_array = np.array(img)
+        return img_array
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    return None
