06435059300c66c8e38f9e9616d6d0483a48aba0
lmunoz-q <lmunoz-q@student.42.fr>
2025-04-18 19:43:14 +0200

    done

--- 1_Array/ex05/pimp_image.py
+++ 1_Array/ex05/pimp_image.py
@@ -4,35 +4,60 @@ import numpy as np
 
 
 def ft_invert(array) -> np.ndarray:
-    invert = array
-    for y in range(len(invert)):
-        for x in range(len(invert[0])):
-            invert[y][x] = 255 - invert[y][x]
-    plt.imshow(invert)
+    for y in range(len(array)):
+        for x in range(len(array[0])):
+            array[y][x] = 255 - array[y][x]
+    plt.imshow(array)
     plt.show()
-    return invert
+    return array
 
 
-#def ft_red(array) -> np.ndarray:
-#    print(array)
+def ft_red(array) -> np.ndarray:
+    for y in range(len(array)):
+        for x in range(len(array[0])):
+            array[y][x][1] = 0
+            array[y][x][2] = 0
+    plt.imshow(array)
+    plt.show()
+    return array
 
 
-#def ft_blue(array) -> array:
-#    print(array)
+def ft_blue(array) -> np.ndarray:
+    for y in range(len(array)):
+        for x in range(len(array[0])):
+            array[y][x][0] = 0
+            array[y][x][1] = 0
+    plt.imshow(array)
+    plt.show()
+    return array
 
 
-#def ft_green(array) -> array:
-#    print(array)
+def ft_green(array) -> np.ndarray:
+    for y in range(len(array)):
+        for x in range(len(array[0])):
+            array[y][x][0] = 0
+            array[y][x][2] = 0
+    plt.imshow(array)
+    plt.show()
+    return array
 
 
-#def ft_gray(array) -> array:
-#    print(array)
+def ft_gray(array) -> np.array:
+    for y in range(len(array)):
+        for x in range(len(array[0])):
+            gray = int(int(array[y][x][0]) + int(array[y][x][1]) + int(array[y][x][2])) / 3
+            array[y][x][0] = np.uint8(gray)
+            array[y][x][1] = np.uint8(gray)
+            array[y][x][2] = np.uint8(gray)
+    plt.imshow(array)
+    plt.show()
+    return array
 
 
 def main():
     path = "landscape.jpg"
     array = ft_load(path)
-    ft_invert(array)
+    ft_gray(array)
 
 
 if __name__ == "__main__":
