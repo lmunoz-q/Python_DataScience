3afc7528a948c28013b415e159ee2f642f92c27f
lmunoz-q <lmunoz-q@student.42.fr>
2025-04-23 00:45:07 +0200

    ex00

--- 2_DataTable/ex00/load_csv.py
+++ 2_DataTable/ex00/load_csv.py
@@ -1,3 +1,11 @@
-import pandas as np
+import pandas as pd
 
-def load(path: str) -> Dataset
+def load(path: str) -> pd.core.frame.DataFrame:
+    try:
+        df = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {df.shape}")
+        return df
+    except Exception as e:
+        print(f"Error : {e}")
+
+    return None
