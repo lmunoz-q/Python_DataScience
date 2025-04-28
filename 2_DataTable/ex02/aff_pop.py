76fa8e1568807e0e8e3e125f824558fc2ff78637
aorus <lmunoz-q@student.42.fr>
2025-04-28 02:27:28 +0200

    adding label

--- 2_DataTable/ex02/aff_pop.py
+++ 2_DataTable/ex02/aff_pop.py
@@ -7,6 +7,8 @@ def main():
     df = load(path)
     my_country = "France"
     other_country = "Belgium"
+    xmin = 1800
+    xmax = 2050
     df_mcountry = df.loc[df['country'] == my_country]
     df_ocountry = df.loc[df['country'] == other_country]
     years = df.columns[1:]
@@ -15,12 +17,13 @@ def main():
         int_years.append(int(i))
     mpopulation = df_mcountry.values[0][1:]
     opopulation = df_ocountry.values[0][1:]
-    plt.plot(int_years, mpopulation)
-    plt.plot(int_years, opopulation)
+    plt.plot(int_years, mpopulation, label=my_country)
+    plt.plot(int_years, opopulation, label=other_country)
+    plt.xlim(xmin, xmax)
     plt.title("Population Projections")
     plt.xlabel("Year")
     plt.ylabel("Population")
-
+    plt.legend()
     plt.show()
 
 
