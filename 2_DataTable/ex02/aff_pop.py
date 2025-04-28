df922dc8875f8190f1c7aca3e739d7fe2ebb4f39
aorus <lmunoz-q@student.42.fr>
2025-04-28 02:45:24 +0200

    too much bugs

--- 2_DataTable/ex02/aff_pop.py
+++ 2_DataTable/ex02/aff_pop.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 def main():
     path = "../ressources/population_total.csv"
     df = load(path)
+    print(df)
     my_country = "France"
-    other_country = "Belgium"
+    other_country = "Zambia"
     xmin = 1800
     xmax = 2050
     df_mcountry = df.loc[df['country'] == my_country]
+    print(df_mcountry)
     df_ocountry = df.loc[df['country'] == other_country]
     years = df.columns[1:]
+    print(years)
     int_years = []
     for i in years:
         int_years.append(int(i))
@@ -20,6 +23,8 @@ def main():
     plt.plot(int_years, mpopulation, label=my_country)
     plt.plot(int_years, opopulation, label=other_country)
     plt.xlim(xmin, xmax)
+    plt.ylim(20, 60)
+    plt.yticks([200, 400, 600], ["20M", "40M", "60M"])
     plt.title("Population Projections")
     plt.xlabel("Year")
     plt.ylabel("Population")
