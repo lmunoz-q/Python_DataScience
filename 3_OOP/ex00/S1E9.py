34c553ffdf0bd96d35503e8f7f95d114ebd95c81
aorus <lmunoz-q@student.42.fr>
2025-05-23 15:24:17 +0200

    bruh

--- 3_OOP/ex00/S1E9.py
+++ 3_OOP/ex00/S1E9.py
@@ -1,9 +1,19 @@
-from abc import ABC, abstractnethod
 
 
-class Character(ABC):
-    abstractnethod
 
+class Stark():
+    """
+    Stark doc
+    """
+    def __init__(self, name):
+        """
+        init doc
+        """
+        self.name = name
+        is_alive = True
 
-class Stark(Character):
-    
+    def die():
+        """
+        killed mf
+        """
+        self.is_alive = False
