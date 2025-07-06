48d3f125acf4761519332be69441a7c476241173
aorus <lmunoz-q@student.42.fr>
2025-07-06 14:59:57 +0200

    done

--- 3_OOP/ex04/ft_calculator.py
+++ 3_OOP/ex04/ft_calculator.py
@@ -1,8 +1,18 @@
 class calculator:
 
+    @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
+        res = 0
+        for i, j in zip(V1, V2):
+            res += i * j
+        print(f"Dot product is: {res}")
 
+    @staticmethod
     def add_vec(V1: list[float], V2: list[float]) -> None:
+        res = [i + j for i, j in zip(V1, V2)]
+        print(f"Add Vector is : {res}")
 
+    @staticmethod
     def sous_vec(V1: list[float], V2: list[float]) -> None:
-
+        res = [i - j for i, j in zip(V1, V2)]
+        print(f"Sous Vector is: {res}")
