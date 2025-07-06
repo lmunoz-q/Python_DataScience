e1a7e507c407f538df161cf5515364ae23cc6a43
aorus <lmunoz-q@student.42.fr>
2025-07-06 13:24:00 +0200

    add class calculator

---
+++ 3_OOP/ex04/ft_calculator.py
@@ -0,0 +1,8 @@
+class calculator:
+
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+
