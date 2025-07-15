8397e3b4fc942c4a10c04fc70221fa25987c509c
aorus <lmunoz-q@student.42.fr>
2025-07-15 15:06:26 +0200

    in out

--- 4_Dod/ex01/in_out.py
+++ 4_Dod/ex01/in_out.py
@@ -1,1 +1,12 @@
-in out
+def square(x : int | float) -> int | float:
+    return x * x
+
+def powww(x : int | float) -> int | float:
+    ret = x
+    for i in range(x - 1):
+        ret *= x
+    return ret
+
+def outer(x : int | float, function) -> objects:
+        count = 0
+    def inner() -> float:
