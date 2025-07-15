9cc83f0412f4528d3d40ef079f866b40de41436a
aorus <lmunoz-q@student.42.fr>
2025-07-15 15:54:54 +0200

    owi

--- 4_Dod/ex01/in_out.py
+++ 4_Dod/ex01/in_out.py
@@ -1,12 +1,25 @@
 def square(x: int | float) -> int | float:
+    """
+    Compute the square of the given number.
+    """
     return x * x
 
 
 def pow(x: int | float) -> int | float:
+    """
+    Compute the exponentiation of x by itself (x ** x).
+    """
     return x ** x
 
 
 def outer(x: int | float, function) -> object:
+    """
+    Return a closure function that applies the given function iteratively
+    to a stored value starting from x.
+
+    Each call to the returned function applies the function to the current
+    value and updates it.
+    """
     value = x
 
     def inner() -> float:
